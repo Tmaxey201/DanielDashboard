82ecc7e6a383ad07153c4b447e5cfb30e51f39e5
Thomas Maxey <tmaxey201@gmail.com>
2022-08-02 12:52:32 -0400

    import appcss and tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import './App.css'
+
 const App = () => {
   return (
-    <div>SOADFJOSDFAOSDKFAOSDKFAOSDK</div>
+    <h1 className='underline text-3x;'>appp</h1>
   )
 }
 
