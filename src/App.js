75da05351541c9167350db0d79f415bbc38ffe36
Thomas Maxey <tmaxey201@gmail.com>
2022-08-03 14:31:20 -0400

    Added routing and toggle sidebar for app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css'
 
 const App = () => {
+  const activeMenu = true;
   return (
-    <h1 className='underline text-3x;'>appp</h1>
+   <div>
+    <BrowserRouter>
+    <div className='flex relative dark:big-main-dark-bg'>
+      <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+        <TooltipComponent content="Settings" position="Top">
+          <button>
+            <FiSettings />
+          </button>
+
+        </TooltipComponent>
+      </div>
+      {activeMenu ? (
+        <div className='w-71 fixed sidebar
+        dark:bg-secondary-dark-bg
+        bg-white'>
+          Sidebar
+          </div>
+      ) : (
+        <div className='w-0 
+        dark:bg-secondary-dark-bg'>
+          Sidebar 
+          </div>
+      )}
+      <div className={
+        `dark:bg-main-bg bg-main-bg
+        min-h-screen w-full ${activeMenu ? 
+        'md:ml-72' : 'flex-2'}`
+        }>
+          <div className='fixed md:static
+          bg-main-bg dark:bg-main-dark-bg
+          navbar w-full'>
+            navbar
+          </div>
+          </div>
+          <div>
+            <Routes>
+              {/* dashboard */}
+              <route path='/' element='dashboard' />
+              <route path='/dashboard' element='dashboard' />
+              {/* pages */}
+              <Route path='/route1' element='route1' />
+              <Route path='/route2' element='route2' />
+              <Route path='/route3' element='route3' />
+            </Routes>
+            </div>
+      </div>
+    </BrowserRouter>
+    </div>
   )
 }
 
